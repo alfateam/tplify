fbd5a2c873ab8c106f19877d92eaaba88485e85a
Adlan Elm <adlan.elm@gmail.com>
2015-09-15 19:16:58 +0200

    make sure that multiple deactivations are ok

--- test/template-engine/view/deactivateSpec.js
+++ test/template-engine/view/deactivateSpec.js
@@ -9,6 +9,13 @@ describe('View', () => {
             it('takes back the the nodes it attached to the region', () => {
                 expect(builder.region.childNodes.length).to.equal(1);
             });
+
+            it('allows multiple deactivations', () => {
+                expect(() => {
+                    sut.deactivate();
+                }).to.not.throw();
+                expect(builder.region.childNodes.length).to.equal(1);
+            });
         });
 
     });
